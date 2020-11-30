c1e9206fd5cf9db6f45f194448310fc385e981b2
soheil rasekh <soheilrasekh555@gmail.com>
2020-11-30 18:34:36 +0330

    just make input range background scale

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //Variables
 let colorDivs = document.querySelectorAll(".color");
 
+//Event Listeners
+
 //Functions
 
 function genreateColor() {
@@ -18,7 +20,38 @@ function setColor() {
 
     hexText.innerHTML = randomColor;
     checkDarkness(randomColor, hexText);
+
+    let color = chroma(randomColor);
+    let inputs = colorDiv.querySelectorAll("input");
+
+    let hue = inputs[0];
+    let brightness = inputs[1];
+    let saturation = inputs[2];
+
+    setAdjust(color, hue, brightness, saturation);
   });
+}
+
+function setAdjust(color, hue, brightness, saturation) {
+  //Hue set
+  hue.style.backgroundImage = `linear-gradient(to right, red 0%, #ff0 17%, lime 33%, cyan 50%, blue 66%, #f0f 83%, red 100%)`;
+
+  //Brightness set
+  let midbrightness = color.set("hsl.l", 0.5);
+  let brightScale = chroma.scale(["black", midbrightness, "white"]);
+  brightness.style.backgroundImage = `linear-gradient(to right,${brightScale(
+    0
+  )},${brightScale(0.5)},${brightScale(1)})`;
+
+  //Saturation set
+  let noSat = color.set("hsl.s", 0);
+  let fullSat = color.set("hsl.s", 1);
+
+  let satScale = chroma.scale([noSat, color, fullSat]);
+
+  saturation.style.backgroundImage = `linear-gradient(to right,${satScale(
+    0
+  )},${satScale(1)})`;
 }
 
 function checkDarkness(color, text) {
